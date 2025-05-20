5704e90d4b649aedeb7a46c1335956d025a1c868
hongryeollim <hongryeollim@lscompany.co.kr>
2025-05-20 18:47:25 +0900

    Chore: 패키지 및 개발환경 설정

---
+++ h-mall/tailwind.config.ts
@@ -0,0 +1,79 @@
+import { heroui } from '@heroui/theme';
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/views/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@heroui/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'hr-purple-default': '#732DCE',
+        'hr-purple-bg': '#F4EFFB',
+        'hr-mint-default': '#01BDD7',
+        'hr-yellow-default': '#FFA203',
+        'hr-pink-default': '#FB1397',
+        'hr-pink-bg': '#FEECF5',
+        'hr-pink-border': '#FBC1E1',
+        'hr-pink-hover': '#F5007A',
+        'hr-danger-default': '#FF2E5F',
+        'hr-danger-bg': '#FFF0F4',
+        'hr-danger-border': '#FFC7D6',
+        'hr-danger-hover': '#F7225A',
+        'hr-success-default': '#2E5AFF',
+        'hr-success-bg': '#F0F0FF',
+        'hr-success-border': '#CDCFFE',
+        'hr-success-hover': '#2656F2',
+        'hr-white': '#FFFFFF',
+        'hr-light': '#F1F1F5',
+        'hr-gray-5': '#F6F7F8',
+        'hr-gray-10': '#EDEFF2',
+        'hr-gray-20': '#E0E2E6',
+        'hr-gray-30': '#D5D7DD',
+        'hr-gray-40': '#C2C5CC',
+        'hr-gray-50': '#878B96',
+        'hr-gray-60': '#5C606A',
+        'hr-gray-70': '#4D525B',
+        'hr-gray-80': '#31353E',
+        'hr-gray-90': '#191B1F',
+        'hr-kakao': '#FEE500',
+        'hr-naver': '#03C75A',
+      },
+      fontFamily: {
+        pretendard: ['var(--font-pretendard)', 'var(--font-notoSansKR)'],
+      },
+      fontSize: {
+        'hr-h2': '2.25rem',
+        'hr-h3': '1.75rem',
+        'hr-h4': '1.5rem',
+        'hr-h5': '1.25rem',
+        'hr-b1': '1.125rem',
+        'hr-b2': '1rem',
+        'hr-b3': '.937rem',
+        'hr-b4': '.875rem',
+        'hr-b5': '.812rem',
+        'hr-c1': '.75rem',
+        'hr-c2': '.687rem',
+        'hr-c3': '.625rem',
+      },
+      fontWeight: {
+        'hr-bold': '700',
+        'hr-semi-bold': '600',
+        'hr-regular': '400',
+      },
+      lineHeight: {
+        'hr-default': '130%',
+        'hr-body': '150%',
+      },
+      padding: {
+        '9': '36px',
+      },
+    },
+  },
+  plugins: [heroui()],
+};
+
+export default config;
