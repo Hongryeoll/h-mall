ac6dc548a09df34e56fa81090b59749b43f5e3c4
hongryeollim <hongryeollim@lscompany.co.kr>
2025-05-21 22:12:56 +0900

    Design: globals css & font 설정

--- h-mall/tailwind.config.ts
+++ h-mall/tailwind.config.ts
@@ -1,12 +1,12 @@
-import { heroui } from '@heroui/theme';
 import type { Config } from 'tailwindcss';
+import { heroui } from '@heroui/theme';
 
 const config: Config = {
   content: [
     './src/views/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@heroui/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
