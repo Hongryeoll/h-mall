620ccf2cb3ce2d54d0f62113ca599088e705f226
hongryeoll <hong462804@gmail.com>
2025-06-27 17:05:02 +0900

    feat: home Promotion Banner 추가

--- h-mall/src/app/page.tsx
+++ h-mall/src/app/page.tsx
@@ -1,11 +1,15 @@
 import '@/app/globals.css';
 import OverlayLogo from '@/components/loader/OverlayLogo';
+import PromotionBanner from '@/components/promotion/PromotionBanner';
 
 export default async function Home() {
   return (
     <>
       <OverlayLogo>
-        <div className="p-4">home화면</div>
+        <div className="p-4">
+          <PromotionBanner />
+          <div className="mt-10">home 화면 콘텐츠 추가 영역</div>
+        </div>
       </OverlayLogo>
     </>
   );
---
+++ h-mall/src/components/promotion/PromotionBanner.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import Image from 'next/image';
+
+const promotionImages = [
+  {
+    id: 1,
+    src: '/images/promotion/promotion-event-1.svg',
+    alt: '기획전 배너 1',
+  },
+  {
+    id: 2,
+    src: '/images/promotion/promotion-event-2.svg',
+    alt: '기획전 배너 2',
+  },
+  {
+    id: 3,
+    src: '/images/promotion/promotion-event-3.svg',
+    alt: '기획전 배너 3',
+  },
+  {
+    id: 4,
+    src: '/images/promotion/promotion-event-4.svg',
+    alt: '기획전 배너 4',
+  },
+  {
+    id: 5,
+    src: '/images/promotion/promotion-event-5.svg',
+    alt: '기획전 배너 5',
+  },
+];
+
+export default function PromotionBanner() {
+  return (
+    <div className="w-full">
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        loop={true}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        pagination={{ clickable: true }}
+        navigation
+        className="w-full h-[400px] rounded-md overflow-hidden"
+      >
+        {promotionImages.map((item) => (
+          <SwiperSlide key={item.id}>
+            <div className="relative w-full h-full">
+              <Image
+                src={item.src}
+                alt={item.alt}
+                fill
+                className="object-cover"
+                priority
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
